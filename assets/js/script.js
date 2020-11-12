a25e01f442d0ea35c7f286722d29af7b550a9b3d
JRob <72467031+Jonathan-84@users.noreply.github.com>
2020-11-12 00:37:52 -0500

    added ability to drag and drop events in different status

--- assets/js/script.js
+++ assets/js/script.js
@@ -31,6 +31,60 @@ var taskP = $("<p>")
 .addClass("m-1")
 .text(text);
 
+//create the sortables
+$('.card .list-group').sortable({
+  connectWith: $('.card .list-group'),
+  scroll: false, 
+  tolerance: "pointer",
+  helper: "clone",
+  activate: function(event){
+    console.log("activate", this);
+},
+deactivate: function(event){
+  console.log("deactivate", this);
+},
+over: function(event){
+  console.log("over", event.target);
+},
+out: function(event){
+  console.log("out", event.target);
+},
+update: function(event){
+  //array to store the task data in
+  var tempArr = [];
+
+ $(this).children().each(function(){
+   var text= $(this)
+   .find("p")
+   .text()
+   .trim();
+
+   var date= $(this)
+   .find("span")
+   .text()
+   .trim();
+   
+   //add task data to temp array as an object
+   tempArr.push({
+     text: text,
+     date: date
+   })
+ });
+
+         //trim down list's ID to match object property
+         var arrName= $(this)
+         .attr("id")
+         .replace("list-", "");
+      
+         //update array on tasks pbject and save
+         tasks[arrName] = tempArr;
+         saveTasks();
+    
+ console.log(tempArr);
+}
+
+  });
+
 //replace textarea with a p element
 $(this).replaceWith(taskP);
 });
