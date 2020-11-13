fbdcd269a771c1971977f1a9fc1b9840575f8295
JRob <72467031+Jonathan-84@users.noreply.github.com>
2020-11-12 23:55:43 -0500

    UX enhancements have been made

--- assets/js/script.js
+++ assets/js/script.js
@@ -47,16 +47,18 @@ $('.card .list-group').sortable({
   tolerance: "pointer",
   helper: "clone",
   activate: function(event){
-    console.log("activate", this);
+    $(this).addClass("dropover")
+    $(".bottom-trash").addClass("bottom-trash-drag")
 },
 deactivate: function(event){
-  console.log("deactivate", this);
+  $(this).removeClass("dropover")
+  $("bttom-trash").removeClass("bottom-trash-drag")
 },
 over: function(event){
-  console.log("over", event.target);
+  $(event.target).addClass("dropover-active")
 },
 out: function(event){
-  console.log("out", event.target);
+  $(event.target).removeClass("dropover-active")
 },
 update: function(event){
   //array to store the task data in
@@ -98,12 +100,15 @@ update: function(event){
     tolerance: "touch",
     drop: function(event, ui) {
       ui.draggable.remove()
+      $(".bottom-trash").removeClass("bottom-trash-active")
       console.log("drop");
     },
     over: function(event, ui) {
+      $(".bottom-trash").addClass("bottom-trash-active")
       console.log("over");
     },
     out: function(event, ui){
+      $(".bottom-trash").removeClass("bottom-trash-active")
       console.log("out");
   }
 });
@@ -237,7 +242,7 @@ $("#task-form-modal").on("shown.bs.modal", function() {
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function() {
+$("#task-form-modal .btn-save").click(function() {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
@@ -270,4 +275,8 @@ $("#remove-tasks").on("click", function() {
 // load tasks for the first time
 loadTasks();
 
-
+setInterval(function() {
+  $(".card .list-group-item").each(function(index, el) {
+    auditTask(el);
+  });
+}, (1000 * 60) * 30);
